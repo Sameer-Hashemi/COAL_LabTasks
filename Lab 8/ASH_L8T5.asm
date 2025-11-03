ca8dc158d8d9ecd55fcd25901113dd7b82c1ef1e
ASHashemi <ashashemi.ai@gmail.com>
2025-11-04 01:23:31 +0500

    Add files via upload

---
+++ Lab 8/ASH_L8T5.asm
@@ -0,0 +1,21 @@
+org 100h
+.data
+a db "SAMEER$"; Name
+.code
+mov ax, @data
+mov ds,ax
+mov si,offset a ;SI points to the start of the string 'S'
+mov cx,6; CX is the loop counter (6 characters: S, A, M, E, E, R)
+L1:
+mov dl, [si]
+inc si
+mov ah, 2
+int 21h   
+loop L1
+
+
+
+
+
+
+
